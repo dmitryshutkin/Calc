032f29760b11e2f280721701f9cb4beb79e52551
Dmitry Shutkin <dmitryshutkin@yandex.ru>
2015-08-28 01:57:17 +0500

    Ввод строки

--- Калькулятор/Калькулятор/Source.cpp
+++ Калькулятор/Калькулятор/Source.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(void)
 {
+	// Русская кодировка
 	setlocale(LC_CTYPE, "");
+	// Получить сторку для парсинга
+	string str = "";
+	cout << "Введите строку: ";
+	cin >> str;
+	cout << "Вы ввели строку: " << str << endl;
+	// Парсинг
+
+	// Возврат управления ОС
 	cout << "Тестовая заглушка. Код возврата -1" << endl;
-	cin.get();
+	cin.get(); cin.get();
 	return -1;
 }
